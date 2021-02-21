400d84ac90436520338338b6c4226a6a76cc62af
William Edson Kilgore IV <2017.william.kilgore@gmail.com>
2021-02-20 17:19:13 -0700

    createUniqueID

--- src/main/java/Calculator.java
+++ src/main/java/Calculator.java
@@ -1,3 +1,4 @@
+import java.util.UUID;
 
 class Calculator {
 
@@ -86,7 +87,7 @@ class Calculator {
      */
     String createUniqueID(String n){
 
-        return null;
+        return n + UUID.randomUUID().toString();
     }
 
 
